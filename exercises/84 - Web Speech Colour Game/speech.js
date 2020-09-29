a6733f974863dcfbc3384893a4584feedf580e94
CarloC24 <carlo.clamucha@gmail.com>
2020-09-29 14:33:45 -0500

    worked on the web colour speech game

--- exercises/84 - Web Speech Colour Game/speech.js
+++ exercises/84 - Web Speech Colour Game/speech.js
@@ -1,33 +1,33 @@
-import { handleResult } from './handlers';
-import { colorsByLength, isDark } from './colors';
+import { handleResult } from "./handlers";
+import { colorsByLength, isDark } from "./colors";
 
-const colorsEl = document.querySelector('.colors');
+const colorsEl = document.querySelector(".colors");
 
 function displayColors(colors) {
   return colors
-    .map(
-      color =>
-        `<span class="color ${color} ${
-          isDark(color) ? 'dark' : ''
-        }" style="background: ${color};">${color}</span>`
-    )
-    .join('');
+    .map(color => {
+      return `<span class="color ${color} ${
+        isDark(color) ? "dark" : ""
+      }" style="background: ${color}">${color}</span>`;
+    })
+    .join("");
 }
 
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
-
 function start() {
-  // see if their browser supports this
-  if (!('SpeechRecognition' in window)) {
-    console.log('Sorry your browser does not support speech reco. ');
+  //see if their browser supports this
+  if (!("SpeechRecognition" in window)) {
+    alert("Sorry your browser does not recognize speech recognition");
     return;
   }
-  // it does work
-  console.log('Starting...');
-  // make a new speech reco
+  //it does work
+  console.log("Starting...");
+  //make a new speech recognition
   const recognition = new SpeechRecognition();
+  //it continuously gets your speech and wont stop listening
   recognition.continuous = true;
+  //give results as you are speaking
   recognition.interimResults = true;
   recognition.onresult = handleResult;
   recognition.start();
